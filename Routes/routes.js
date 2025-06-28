83ab38014d2f729eb5260240b41f6d31c7a9a90e
geoffowuor <geoffreygoovy@gmail.com>
2025-06-28 14:07:56 +0300

    added delete function

--- Routes/routes.js
+++ Routes/routes.js
@@ -62,10 +62,9 @@ router.put('/tasks/:id', async (req, res) =>{
 //delete
 router.delete('/tasks/:id', async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(
-            req.params.id,
-            req.body,
-        )
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if (!task) return res.status(404).send();
+        res.send(task);
     }catch(error){
         res.status(500).send(error);
     }
